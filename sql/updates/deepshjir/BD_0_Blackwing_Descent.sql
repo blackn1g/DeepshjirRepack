48169a9a6580d1783a8bc35bdf7136746fef892a
Naios <naios-dev@live.de>
2012-02-04 10:05:55 +0100

    Scripts/BWD: Some workarround on BWD
    Signed-off-by: Naios <naios-dev@live.de>

--- sql/updates/deepshjir/BD_0_Blackwing_Descent.sql
+++ sql/updates/deepshjir/BD_0_Blackwing_Descent.sql
@@ -5,7 +5,7 @@
 -- Blackwing Descent Misc
  
 DELETE FROM `gameobject` WHERE `guid` IN(189283);
-DELETE FROM `gameobject` WHERE `id` IN(/*Deletes gobs that are spawned through this script*/207834, 402092, 204929, 203306,/*Deletes unofficial gobs*/ 202211, 205079, 24388, 203083, 207207) AND `map` = 669;
+DELETE FROM `gameobject` WHERE `id` IN(/*Deletes gobs that are spawned through this script*/207834, 402092, 204929, 203306,/*Deletes unofficial gobs*/ 202211, 205079, 24388, 203083, 20720, 402368) AND `map` = 669;
 
 INSERT INTO `gameobject` (`id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES
 -- Onyxia Elevator
@@ -15,7 +15,11 @@ INSERT INTO `gameobject` (`id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `
 -- Finkles Cage
 (204929, 669, 15, 1, -115.558, 45.1754, 71, 3.05438, 0, 0, 0.999049, 0.0435902, 300, 0, 1),
 -- Maloriaks Cauldron
-(203306, 669, 15, 1, -105.803, -485.729, 73.4583, 3.08822, 0, 0, 0.999644, 0.0266813, 300, 0, 1);
+(203306, 669, 15, 1, -105.803, -485.729, 73.4583, 3.08822, 0, 0, 0.999644, 0.0266813, 300, 0, 1),
+
+-- Atramedes Door
+(402368, 669, 15, 1, 53.670784, -224.593292, 94.5037, 3.130350, 0, 0, 0.999629, 0.0272188, 300, 0, 1);
+
 
 DELETE FROM `creature` WHERE `id` IN(44418, 44202) AND `map` = 669;
 INSERT INTO `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES
@@ -31,15 +35,7 @@ REPLACE INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid
 REPLACE INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `faction`, `flags`, `size`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `data24`, `data25`, `data26`, `data27`, `data28`, `data29`, `data30`, `data31`, `AIName`, `ScriptName`, `WDBVerified`) VALUES
 (402092, 0, 9946, 'BLACKROCKV2_PORTCULLIS_02', '','','',1375, 32, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', '', 13623),
 (203716, 11, 10407, 'Blackwing Descent Elevator', '', '', '', 0, 40, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 13623),
-(207834, 11, 10363, 'Doodad_BlackWingV2_Elevator_Onyxia01', '', '', '', 0, 40, 1, 0, 0, 0, 0, 0, 0, 13333, 0, 0, 0, 0, 748, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 13623);
+(207834, 11, 10363, 'Doodad_BlackWingV2_Elevator_Onyxia01', '', '', '', 0, 40, 1, 0, 0, 0, 0, 0, 0, 13333, 0, 0, 0, 0, 748, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 13623),
+(402368, 0, 10463, 'BLACKWING_PORTCULLIS', '', '', '', 1375, 32, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', '', 13623);
 
 UPDATE `gameobject` SET `spawnMask` = 15 WHERE `map` = 669;
-
--- elevator 205079
-
-/*
-INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `faction`, `flags`, `size`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `data24`, `data25`, `data26`, `data27`, `data28`, `data29`, `data30`, `data31`, `AIName`, `ScriptName`, `WDBVerified`) VALUES
-
-(205079, 11, 807, 'GnomeElevator01', '', '', '', 0, 40, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', '', 1);
-
-*/
--- sql/updates/deepshjir/BD_7_Trash.sql
+++ sql/updates/deepshjir/BD_7_Trash.sql
@@ -15,8 +15,8 @@ SET @SOURCETYPE := 0;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY;
 UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
-(@ENTRY,@SOURCETYPE,0,0,0,0,100,30,15000,15000,23000,24000,11,81055,1,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"Golem Sentry - Electrical Discharge"),
-(@ENTRY,@SOURCETYPE,1,0,0,0,100,30,7000,7000,16000,18000,11,81056,1,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"Golem Sentry - Flash Bomb"),
+(@ENTRY,@SOURCETYPE,0,0,0,0,100,30,15000,15000,21000,24000,11,81055,1,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"Golem Sentry - Electrical Discharge"),
+(@ENTRY,@SOURCETYPE,1,0,0,0,100,30,7000,7000,13000,16000,11,81056,1,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"Golem Sentry - Flash Bomb"),
 (@ENTRY,@SOURCETYPE,2,0,0,0,100,30,12000,14000,38000,42000,11,81063,1,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"Golem Sentry - Laser Strike");
 
 -- Flash Bomb
@@ -36,7 +36,7 @@ DELETE FROM `creature_template_addon` WHERE `entry` = 43362;
 INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`)
 VALUES (43362, 0, 0, 0, 0, 0, '81065 0 81066 0');
 
-UPDATE `creature_template` SET `unit_flags`=33554436, `type_flags`=1024, `faction_H`=16, `faction_A`=16, AIName="SmartAI" WHERE `entry`=43362 LIMIT 1;
+UPDATE `creature_template` SET `unit_flags`=33554436, `type_flags`=1024, `faction_H`=16, `faction_A`=16, AIName="" WHERE `entry`=43362 LIMIT 1;
 
 /*****
 * Drakonid Chainwielder
