2a71a3effa07c3e5701c6d42669038c18870970c
Naios <naios-dev@live.de>
2012-02-24 04:02:30 +0100

    Core/Scripts: Chimaeron: Fixed waypoints of Bile-O-Tron and finkles mixture
    Signed-off-by: Naios <naios-dev@live.de>

--- src/server/scripts/EasternKingdoms/BlackwingDescent/boss_chimaeron.cpp
+++ src/server/scripts/EasternKingdoms/BlackwingDescent/boss_chimaeron.cpp
@@ -1,26 +1,31 @@
 /*
- * Copyright (C) 2005 - 2011 MaNGOS <http://www.getmangos.org/>
- *
- * Copyright (C) 2008 - 2011 TrinityCore <http://www.trinitycore.org/>
- *
- * Copyright (C) 2011 - 2012 ArkCORE <http://www.arkania.net/>
- *
- * Copyright (C) 2012 DeepshjirCataclysm Repack
- * By Naios
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+* Copyright (C) 2005 - 2011 MaNGOS <http://www.getmangos.org/>
+*
+* Copyright (C) 2008 - 2011 TrinityCore <http://www.trinitycore.org/>
+*
+* Copyright (C) 2011 - 2012 ArkCORE <http://www.arkania.net/>
+*
+* Copyright (C) 2012 DeepshjirCataclysm Repack
+* By Naios
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation; either version 2 of the License, or (at your
+* option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+*This script is arround 93% complete...
+*Hardmode and Double Attack Spellscript must be written.
+*/
 
 #include "ScriptPCH.h"
 #include "blackwing_descent.h"
@@ -56,6 +61,9 @@ enum Spells
     SPELL_FINKLES_MIXTURE_VISUAL        = 91106,
     SPELL_SYSTEM_FALURE                 = 88853,
     SPELL_REROUTE_POWER                 = 88861,
+
+    // HC: Nefarian
+    SPELL_MOCKING_SHADOWS               = 91307,
 };
 
 Position const BilePositions[6] =
@@ -80,23 +88,24 @@ public:
 
     struct boss_chimaeronAI: public BossAI
     {
-		boss_chimaeronAI(Creature* creature) : BossAI(creature, DATA_CHIMAERON)
+        boss_chimaeronAI(Creature* creature) : BossAI(creature, DATA_CHIMAERON)
         {
             instance = creature->GetInstanceScript();
         }
 
         InstanceScript* instance;
-		EventMap events;
+        EventMap events;
         uint8 phase;
-		
+
         void Reset()
-		{
-			events.Reset();
+        {
+            events.Reset();
             me->SetReactState(REACT_PASSIVE);
             phase = 1;
 
             me->RemoveAura(SPELL_DOUBLE_ATTACK);
             me->RemoveAura(SPELL_MORTALITY);
+            me->RemoveAura(SPELL_MOCKING_SHADOWS);
 
             if(Creature* finkle_einhorn = ObjectAccessor::GetCreature(*me,instance->GetData64(NPC_FINKLE_EINHORN)))
                 finkle_einhorn->SetFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP);
@@ -105,52 +114,55 @@ public:
                 bile_o_tron->AI()->DoAction(ACTION_BILE_O_TRON_RESET);
 
             _Reset();
-		}
+        }
 
         void EnterCombat(Unit* /*who*/)
-		{		
+        {
+            if(me->GetMap()->IsHeroic())
+                DoCast(me, SPELL_MOCKING_SHADOWS);
+
             events.ScheduleEvent(EVENT_MASSACRE, urand(30000,35000));
-			events.ScheduleEvent(EVENT_DOUBLE_ATTACK, urand(13000,15000));
+            events.ScheduleEvent(EVENT_DOUBLE_ATTACK, urand(13000,15000));
             events.ScheduleEvent(EVENT_CAUSTIC_SLIME, urand(10000,12000));
             events.ScheduleEvent(EVENT_BREAK, urand(14000,16000));
 
             _EnterCombat();
-		}
+        }
 
         void UpdateAI(const uint32 diff)
-		{
-			if (!UpdateVictim() || me->HasUnitState(UNIT_STAT_CASTING))
-				return;
-			
+        {
+            if (!UpdateVictim() || me->HasUnitState(UNIT_STAT_CASTING))
+                return;
+
             if(me->GetHealthPct() < 20 && phase == 1)
             {
                 phase = 2;
 
-                DoCast(me,SPELL_MORTALITY);
+                DoCast(me, SPELL_MORTALITY);
 
                 events.CancelEvent(EVENT_MASSACRE);
                 events.CancelEvent(EVENT_BREAK);
                 events.CancelEvent(EVENT_CAUSTIC_SLIME);
             }
 
-			events.Update(diff);
+            events.Update(diff);
             _DoAggroPulse(diff);
 
-			while (uint32 eventId = events.ExecuteEvent())
-			{
-				switch (eventId)
-				{
+            while (uint32 eventId = events.ExecuteEvent())
+            {
+                switch (eventId)
+                {
 
-				case EVENT_MASSACRE:
-					DoCastVictim(SPELL_MASSACRE);
+                case EVENT_MASSACRE:
+                    DoCastVictim(SPELL_MASSACRE);
                     DoCast(me,SPELL_FEUD);
 
                     if(Creature* bile_o_tron = ObjectAccessor::GetCreature(*me,instance->GetData64(NPC_BILE_O_TRON)))
                         bile_o_tron->AI()->DoAction(ACTION_BILE_O_TRON_SYSTEM_FAILURE);
 
-					events.ScheduleEvent(EVENT_MASSACRE, urand(90000,95000));
-					break;
-				
+                    events.ScheduleEvent(EVENT_MASSACRE, urand(90000,95000));
+                    break;
+
                 case EVENT_DOUBLE_ATTACK:
                     DoCast(me, SPELL_DOUBLE_ATTACK);
                     events.ScheduleEvent(EVENT_DOUBLE_ATTACK, urand(13000,15000));
@@ -166,16 +178,16 @@ public:
                     events.ScheduleEvent(EVENT_BREAK, 14000);
                     break;
 
-				default:
-					break;
-				}
-			}		
+                default:
+                    break;
+                }
+            }		
 
             DoMeleeAttackIfReady();
         }
 
         void DamageTaken(Unit* who, uint32& damage)
-		{
+        {
             if(me->HasReactState(REACT_PASSIVE))
             {
                 me->SetReactState(REACT_AGGRESSIVE);
@@ -199,22 +211,22 @@ public:
 class mob_finkle_einhorn : public CreatureScript
 {
 public:
-	mob_finkle_einhorn() : CreatureScript("mob_finkle_einhorn") { }
+    mob_finkle_einhorn() : CreatureScript("mob_finkle_einhorn") { }
 
-	bool OnGossipHello(Player* pPlayer, Creature* creature)
-	{
+    bool OnGossipHello(Player* pPlayer, Creature* creature)
+    {
 
         pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Help us with your Bille-O-Tron 800!", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
-		pPlayer->SEND_GOSSIP_MENU(1,creature->GetGUID());
+        pPlayer->SEND_GOSSIP_MENU(1,creature->GetGUID());
 
-		return true;
-	}
+        return true;
+    }
 
-	bool OnGossipSelect(Player* pPlayer, Creature* creature, uint32 uiSender, uint32 uiAction)
-	{
-		pPlayer->PlayerTalkClass->ClearMenus();
+    bool OnGossipSelect(Player* pPlayer, Creature* creature, uint32 uiSender, uint32 uiAction)
+    {
+        pPlayer->PlayerTalkClass->ClearMenus();
 
-		pPlayer->CLOSE_GOSSIP_MENU();
+        pPlayer->CLOSE_GOSSIP_MENU();
 
         if(InstanceScript* instance = creature->GetInstanceScript())
         {
@@ -224,8 +236,8 @@ public:
                 creature->RemoveFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP);
             }
         }
-		return true;
-	}
+        return true;
+    }
 };
 
 class mob_bile_o_tron : public CreatureScript
@@ -240,54 +252,56 @@ public:
 
     struct mob_bile_o_tronAI : public ScriptedAI
     {
-        mob_bile_o_tronAI(Creature* creature) : ScriptedAI(creature), Waypoint(7), uiSystemFailureTimer(0), activated(false)
+        mob_bile_o_tronAI(Creature* creature) : ScriptedAI(creature), waypoint(7), uiSystemFailureTimer(0), activated(false)
         {
             instance = creature->GetInstanceScript();
             creature->AddUnitMovementFlag(MOVEMENTFLAG_WALKING);
+
+            isFailureActive = false;
         }
 
         InstanceScript* instance;
-        uint8 Waypoint;
+        uint8 waypoint;
         uint32 uiSystemFailureTimer;
         bool activated;
-        		
+        bool isFailureActive;
+
         void UpdateAI(const uint32 diff)
-		{
-            if(uiSystemFailureTimer == 0 || !activated)
+        {
+            if(!activated)
                 return;
 
-            if(uiSystemFailureTimer <= diff)
-            {
-                me->RemoveAura(SPELL_SYSTEM_FALURE);
-                me->GetMotionMaster()->MovePoint(1,BilePositions[Waypoint]);
-                uiSystemFailureTimer = 0;
-                
-                DoCast(me,SPELL_FINKLES_MIXTURE_VISUAL,true);
+            if(!me->HasAura(SPELL_SYSTEM_FALURE) && !me->HasAura(SPELL_FINKLES_MIXTURE))
+                me->AddAura(SPELL_FINKLES_MIXTURE, me);
 
-                if(instance)
-                    instance->DoCastSpellOnPlayers(SPELL_FINKLES_MIXTURE);
+            if(uiSystemFailureTimer <= diff && isFailureActive)
+            { // Reroute Power
+                me->RemoveAura(SPELL_SYSTEM_FALURE);
+                me->GetMotionMaster()->MovePoint(1,BilePositions[waypoint]);
+
+                isFailureActive = false;
+                DoCast(me,SPELL_FINKLES_MIXTURE_VISUAL,true);
             }
             else uiSystemFailureTimer -= diff;
         }
 
         void DoAction(const int32 action)
         {
+
+            Map::PlayerList const &PlayerList = me->GetMap()->GetPlayers();
+
             switch(action)
             {
-            
+
             case ACTION_BILE_O_TRON_EVENT_START:
                 DoCast(me,SPELL_FINKLES_MIXTURE_VISUAL,true);
-
-                if(instance)
-                    instance->DoCastSpellOnPlayers(SPELL_FINKLES_MIXTURE);
-
-                Waypoint = 8;
-                me->GetMotionMaster()->MovePoint(1,BilePositions[0]);
+                waypoint = 8;
+                me->GetMotionMaster()->MovePoint(1, BilePositions[0]);
                 activated = true;
+                isFailureActive = false;
                 break;
 
             case ACTION_BILE_O_TRON_SYSTEM_FAILURE:
-                
                 if(!activated)
                     break;
 
@@ -297,35 +311,36 @@ public:
                 me->RemoveAllAuras();
                 DoCast(me,SPELL_REROUTE_POWER, true);
                 DoCast(me,SPELL_SYSTEM_FALURE, true);
-                
+                isFailureActive = true;
                 uiSystemFailureTimer = 26000;
-
-                if(instance)
-                    instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_FINKLES_MIXTURE);
-
                 break;
 
             case ACTION_BILE_O_TRON_RESET:
                 me->RemoveAllAuras();
                 me->GetMotionMaster()->MoveTargetedHome();
-                Waypoint = 7;
+                waypoint = 7;
                 uiSystemFailureTimer = 0;
                 activated = false;
                 break;
             }
         }
 
+        void JustReachedHome()
+        {
+            me->RemoveAllAuras();
+        }
+
         void MovementInform(uint32 type, uint32 id)
-		{
-			if (type != POINT_MOTION_TYPE || Waypoint == 7)
-				return;
+        {
+            if (type != POINT_MOTION_TYPE || waypoint == 7)
+                return;
 
-            if(Waypoint <= 5)
-                Waypoint++;
+            if(waypoint >= 5)
+                waypoint = 0;
             else
-                Waypoint = 0;
+                waypoint++;
 
-            me->GetMotionMaster()->MovePoint(1,BilePositions[Waypoint]);               
+            me->GetMotionMaster()->MovePoint(1,BilePositions[waypoint]);               
         }
     };
 };
