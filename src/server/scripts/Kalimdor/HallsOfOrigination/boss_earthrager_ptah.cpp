067f37477203de284f1a83a7fd4e8862d8ba5bd8
Naios <naios-dev@live.de>
2012-03-01 16:15:10 +0100

    Core/Scripts: HoO: Updated Earthrager Ptah fixed Built
    Signed-off-by: Naios <naios-dev@live.de>

--- src/server/scripts/Kalimdor/HallsOfOrigination/boss_earthrager_ptah.cpp
+++ src/server/scripts/Kalimdor/HallsOfOrigination/boss_earthrager_ptah.cpp
@@ -1,22 +1,22 @@
 /*
- * Copyright (C) 2010-2012 Project SkyFire <http://www.projectskyfire.org/>
- * Copyright (C) 2011-2012 ArkCORE <http://www.arkania.net/>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+* Copyright (C) 2010-2012 Project SkyFire <http://www.projectskyfire.org/>
+* Copyright (C) 2011-2012 ArkCORE <http://www.arkania.net/>
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation; either version 2 of the License, or (at your
+* option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
- #include "ScriptPCH.h"
+#include "ScriptPCH.h"
 #include "halls_of_origination.h"
 
 enum Texts
@@ -26,7 +26,7 @@ enum Texts
     SAY_CAST        = 2,    //Dust to dust.
 };
 
-enum CreatureIds
+enum Npcs
 {
     BOSS_PTAH       = 39428,
     NPC_HORROR      = 40810,
@@ -68,107 +68,111 @@ const Position aSpawnLocations[3] =
 
 class boss_ptah : public CreatureScript
 {
-    public:
-        boss_ptah() : CreatureScript("boss_ptah") {}
+public:
+    boss_ptah() : CreatureScript("boss_ptah") {}
 
-        struct boss_ptahAI : public BossAI
+    struct boss_ptahAI : public BossAI
+    {
+        boss_ptahAI(Creature* creature) : BossAI(creature, DATA_EARTHRAGER_PTAH_EVENT), Summons(me)
         {
-            boss_ptahAI(Creature* creature) : BossAI(creature), Summons(me)
+            instance = creature->GetInstanceScript();
+        }
+
+        InstanceScript* instance;
+        SummonList Summons;
+
+        void Reset()
+        {
+            _Reset();
+        }
+
+        void EnterCombat(Unit * /*who*/)
+        {
+            _EnterCombat();
+
+            events.ScheduleEvent(EVENT_FLAME_BOLT, 7500);
+            events.ScheduleEvent(EVENT_RAGING_SMASH, urand(4000, 10000));
+            events.ScheduleEvent(EVENT_EARTH_POINT, 8000);
+            events.ScheduleEvent(EVENT_SUMMON, 50000);
+            events.ScheduleEvent(EVENT_DUST_MOVE, 15000);
+            events.ScheduleEvent(EVENT_VORTEX_DUST, urand(14000, 20000));
+
+            Talk(SAY_AGGRO);
+            instance->SendEncounterUnit(ENCOUNTER_FRAME_ADD, me);
+        }
+
+        void JustDied(Unit* /*killer*/)
+        {
+            _JustDied();
+
+            Talk(SAY_DEATH);
+            instance->SendEncounterUnit(ENCOUNTER_FRAME_REMOVE, me);
+        }
+
+        void JustSummoned(Creature *summoned)
+        {
+            summoned->SetInCombatWithZone();
+            if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM,0))
+                summoned->AI()->AttackStart(target);
+            Summons.Summon(summoned);
+        }
+
+        void SummonedCreatureDespawn(Creature* summon)
+        {
+            Summons.Despawn(summon);
+        }
+
+        void UpdateAI(const uint32 diff)
+        {
+            if (!UpdateVictim())
+                return;
+
+            events.Update(diff);
+
+            while (uint32 eventId = events.ExecuteEvent())
             {
-                instance = creature->GetInstanceScript();
-            }
-
-            InstanceScript* instance;
-            EventMap events;
-            SummonList Summons;
-
-            void EnterCombat(Unit * /*who*/)
-            {
-                EnterPhaseGround();
-                Talk(SAY_AGGRO);
-                instance->SendEncounterUnit(ENCOUNTER_FRAME_ADD, me);
-            }
-
-            void JustDied(Unit* /*killer*/)
-            {
-                Talk(SAY_DEATH);
-                instance->SendEncounterUnit(ENCOUNTER_FRAME_REMOVE, me);
-            }
-
-            void JustSummoned(Creature *summoned)
-            {
-                summoned->SetInCombatWithZone();
-                if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM,0))
-                    summoned->AI()->AttackStart(target);
-                Summons.Summon(summoned);
-            }
-
-            void SummonedCreatureDespawn(Creature* summon)
-            {
-                Summons.Despawn(summon);
-            }
-
-            void EnterPhaseGround()
-            {
-                events.ScheduleEvent(EVENT_FLAME_BOLT, 7500);
-                events.ScheduleEvent(EVENT_RAGING_SMASH, urand(4000, 10000));
-                events.ScheduleEvent(EVENT_EARTH_POINT, 8000);
-                events.ScheduleEvent(EVENT_SUMMON, 50000);
-                events.ScheduleEvent(EVENT_DUST_MOVE, 15000);
-                events.ScheduleEvent(EVENT_VORTEX_DUST, urand(14000, 20000));
-            }
-
-            void UpdateAI(const uint32 diff)
-            {
-                if (!UpdateVictim())
-                    return;
-
-                events.Update(diff);
-
-                while (uint32 eventId = events.ExecuteEvent())
+                switch (eventId)
                 {
-                    switch (eventId)
+                case EVENT_FLAME_BOLT:
+                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, true))
+                        DoCast(target, SPELL_FLAME_BOLT);
+                    events.ScheduleEvent(EVENT_FLAME_BOLT, 7500);
+                    break;;
+                case EVENT_RAGING_SMASH:
+                    DoCast(me->getVictim(), SPELL_RAGING_SMASH);
+                    events.ScheduleEvent(EVENT_RAGING_SMASH, urand(4000, 10000));
+                    break;;
+                case EVENT_EARTH_POINT:
+                    Talk(SAY_CAST);
+                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM,1,100,true))
+                        DoCast(target, SPELL_EARTH_PEAK);
+                    events.ScheduleEvent(EVENT_EARTH_POINT, 8000);
+                    break;;
+                case EVENT_SUMMON:
+                    if (IsHeroic())
                     {
-                        case EVENT_FLAME_BOLT:
-                            if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, true))
-                                DoCast(target, SPELL_FLAME_BOLT);
-                            events.ScheduleEvent(EVENT_FLAME_BOLT, 7500);
-                            break;;
-                        case EVENT_RAGING_SMASH:
-                            DoCast(me->getVictim(), SPELL_RAGING_SMASH);
-                            events.ScheduleEvent(EVENT_RAGING_SMASH, urand(4000, 10000));
-                            break;;
-                        case EVENT_EARTH_POINT:
-                            Talk(SAY_CAST);
-                            if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM,1,100,true))
-                                DoCast(target, SPELL_EARTH_PEAK);
-                            events.ScheduleEvent(EVENT_EARTH_POINT, 8000);
-                            break;;
-                        case EVENT_SUMMON:
-                            if (IsHeroic())
-                            {
-                                me->SummonCreature(NPC_HORROR, aSpawnLocations[0].GetPositionX(), aSpawnLocations[0].GetPositionY(), aSpawnLocations[0].GetPositionZ(), 0.0f, TEMPSUMMON_CORPSE_DESPAWN);
-                                me->SummonCreature(NPC_SCARAB, aSpawnLocations[1].GetPositionX(), aSpawnLocations[1].GetPositionY(), aSpawnLocations[1].GetPositionZ(), 0.0f, TEMPSUMMON_CORPSE_DESPAWN);
-                                events.ScheduleEvent(EVENT_SUMMON, 50000);
-                            }
-                            break;
-                        case EVENT_DUST_MOVE:
-                            if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, true))
-                                DoCast(target, SPELL_DUST_MOVE);
-                            events.ScheduleEvent(EVENT_DUST_MOVE, 15000);
-                            break;
-                        case EVENT_VORTEX_DUST:
-                            if (IsHeroic())
-                            {
-                                DoCastAOE(SPELL_VORTEX_DUST);
-                            }
-                            events.ScheduleEvent(EVENT_VORTEX_DUST, urand(14000, 20000));
-                            break;
+                        me->SummonCreature(NPC_HORROR, aSpawnLocations[0].GetPositionX(), aSpawnLocations[0].GetPositionY(), aSpawnLocations[0].GetPositionZ(), 0.0f, TEMPSUMMON_CORPSE_DESPAWN);
+                        me->SummonCreature(NPC_SCARAB, aSpawnLocations[1].GetPositionX(), aSpawnLocations[1].GetPositionY(), aSpawnLocations[1].GetPositionZ(), 0.0f, TEMPSUMMON_CORPSE_DESPAWN);
+                        events.ScheduleEvent(EVENT_SUMMON, 50000);
                     }
+                    break;
+                case EVENT_DUST_MOVE:
+                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, true))
+                        DoCast(target, SPELL_DUST_MOVE);
+                    events.ScheduleEvent(EVENT_DUST_MOVE, 15000);
+                    break;
+                case EVENT_VORTEX_DUST:
+                    if (IsHeroic())
+                    {
+                        DoCastAOE(SPELL_DUST_VORTEX);
+                    }
+                    events.ScheduleEvent(EVENT_VORTEX_DUST, urand(14000, 20000));
+                    break;
                 }
-                DoMeleeAttackIfReady();
             }
-        };
+            DoMeleeAttackIfReady();
+        }
+    };
 
     CreatureAI* GetAI(Creature* creature) const
     {
@@ -178,45 +182,48 @@ class boss_ptah : public CreatureScript
 
 class npc_horror : public CreatureScript
 {
-    public:
-        npc_horror() : CreatureScript("npc_horror") {}
+public:
+    npc_horror() : CreatureScript("npc_horror") {}
 
-        struct npc_horrorAI : public ScriptedAI
+    struct npc_horrorAI : public ScriptedAI
+    {
+        npc_horrorAI(Creature* creature) : ScriptedAI(creature)
         {
-            npc_horrorAI(Creature* creature) : ScriptedAI(creature)
-            {
-                instance = creature->GetInstanceScript();
-            }
-
-            void EnterCombat(Unit * /*who*/)
-            {
-                events.ScheduleEvent(EVENT_CRUNCH, urand(6000, 7000));
-            }
-
-            void UpdateAI(const uint32 diff)
-            {
-                if (!UpdateVictim())
-                    return;
-
-                events.Update(diff);
-
-                while (uint32 eventId = events.ExecuteEvent())
-                {
-                    switch (eventId)
-                    {
-                        case EVENT_CRUNCH:
-                            DoCast(me->getVictim(), SPELL_CRUNCH);
-                            events.ScheduleEvent(EVENT_CRUNCH, urand(6000, 7000);
-                            break;
-                    }
-                }
-                DoMeleeAttackIfReady();
-            }
-        };
-        CreatureAI* GetAI(Creature* creature) const
-        {
-            return new npc_horrorAI(creature);
+            instance = creature->GetInstanceScript();
         }
+
+        EventMap events;
+        InstanceScript* instance;
+
+        void EnterCombat(Unit * /*who*/)
+        {
+            events.ScheduleEvent(EVENT_CRUNCH, urand(6000, 7000));
+        }
+
+        void UpdateAI(const uint32 diff)
+        {
+            if (!UpdateVictim())
+                return;
+
+            events.Update(diff);
+
+            while (uint32 eventId = events.ExecuteEvent())
+            {
+                switch (eventId)
+                {
+                case EVENT_CRUNCH:
+                    DoCast(me->getVictim(), SPELL_CRUNCH);
+                    events.ScheduleEvent(EVENT_CRUNCH, urand(6000, 7000));
+                    break;
+                }
+            }
+            DoMeleeAttackIfReady();
+        }
+    };
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new npc_horrorAI(creature);
+    }
 };
 
 void AddSC_boss_ptah()
